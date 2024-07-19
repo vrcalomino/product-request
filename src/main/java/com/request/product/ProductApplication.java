0ab5874942947f756a7a320344405fb9771f1b55
vrcalomino <vrcalomino@gmail.com>
2024-07-19 10:45:05 -0300

    add connection to db

--- src/main/java/com/request/product/ProductApplication.java
+++ src/main/java/com/request/product/ProductApplication.java
@@ -1,12 +1,20 @@
 package com.request.product;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Objects;
 
 @SpringBootApplication
 public class ProductApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+
+		System.setProperty("spring.datasource.username", Objects.requireNonNull(dotenv.get("MYSQL_USER")));
+		System.setProperty("spring.datasource.password", Objects.requireNonNull(dotenv.get("MYSQL_PASSWORD")));
+		System.setProperty("spring.datasource.url", Objects.requireNonNull(dotenv.get("MYSQL_URL")));
 		SpringApplication.run(ProductApplication.class, args);
 	}
 
